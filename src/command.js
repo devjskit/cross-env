e9d3abb565538eaf5f746f738004eb61643b3bb4
Hi.Billgo <cocobill@vip.qq.com>
2023-09-30 12:05:17 +0800

    update

--- src/command.js
+++ src/command.js
@@ -18,7 +18,15 @@ function commandConvert(command, env, normalize = false) {
   const envUnixRegex = /\$(\w+)|\${(\w+)}/g // $my_var or ${my_var}
   const convertedCmd = command.replace(envUnixRegex, (match, $1, $2) => {
     const varName = $1 || $2
+    // In Windows, non-existent variables are not replaced by the shell,
+    // so for example "echo %FOO%" will literally print the string "%FOO%", as
+    // opposed to printing an empty string in UNIX. See kentcdodds/cross-env#145
+    // If the env variable isn't defined at runtime, just strip it from the command entirely
     return env[varName] ? `%${varName}%` : ''
   })
+  // Normalization is required for commands with relative paths
+  // For example, `./cmd.bat`. See kentcdodds/cross-env#127
+  // However, it should not be done for command arguments.
+  // See https://github.com/kentcdodds/cross-env/pull/130#issuecomment-319887970
   return normalize === true ? path.normalize(convertedCmd) : convertedCmd
 }
